610ded5d2767ba448dacd2b503b57d602ffa12cf
jesus <jesusre890@gmail.com>
2022-08-25 21:43:26 -0300

    primer prompt y alert

---
+++ main.js
@@ -0,0 +1,14 @@
+let nombreIngresado = prompt('Ingrese su nombre');
+let apellidoIngresado = prompt('Ingrese su apellido');
+const de = 'de';
+const signoDeAdmiracion = '!';
+let paisIngresado = prompt('Ingrese su pais');
+const BLANCO = ' ';
+
+
+console.log(nombreIngresado);
+console.log(apellidoIngresado);
+console.log(paisIngresado);
+
+// alert('Bienvenido '+nombreIngresado+BLANCO+apellido+BLANCO+Ingresado+BLANCO+de+BLANCO+paisIngresado+signoDeAdmiracion);
+alert('Bienvenido '+nombreIngresado+BLANCO+apellidoIngresado+BLANCO+de+BLANCO+paisIngresado+signoDeAdmiracion);
